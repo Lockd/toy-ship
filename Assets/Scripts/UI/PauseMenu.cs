e215ae9af51a823b3f07338ce2b72f269e4875c6
Lockd <pikagrigoriy@gmail.com>
2021-11-21 18:32:30 +0300

    added countwond on resuming game

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -8,13 +8,47 @@ public class PauseMenu : MonoBehaviour
     public static bool isGamePaused = false;
     public GameObject ingameUI;
     public Text countdownText;
+    private bool isCountingDown = false;
+    private float countdownLength;
+    private bool isInCoroutine = false;
 
     void Awake()
     {
         countdownText.gameObject.SetActive(false);
+        countdownLength = 3;
     }
+
+    void Update()
+    {
+        if (isCountingDown && !isInCoroutine)
+        {
+            StartCoroutine(countDown());
+        }
+    }
+
+    IEnumerator countDown() {
+        if (countdownLength == 0) {
+            getBackToScene();
+        }
+        isInCoroutine = true;
+        countdownText.text = countdownLength.ToString("f0");
+        
+        countdownLength -= 1;
+        yield return new WaitForSecondsRealtime(0.7f);
+        isInCoroutine = false;
+    }
+
     public void Resume()
     {
+        isCountingDown = true;
+        countdownText.gameObject.SetActive(true);
+    }
+
+    public void getBackToScene()
+    {
+        countdownText.gameObject.SetActive(false);
+        countdownLength = 3;
+        isCountingDown = false;
         Time.timeScale = 1f;
         gameObject.SetActive(false);
         isGamePaused = false;
