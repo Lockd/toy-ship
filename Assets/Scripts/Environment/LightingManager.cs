3a798a99a1342b022537e2119b8a387f08d54772
Lockd <pikagrigoriy@gmail.com>
2021-11-27 23:12:44 +0300

    added day and night cycle, skybox and fog

---
+++ Assets/Scripts/Environment/LightingManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class LightingManager : MonoBehaviour
+{
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingPreset Preset;
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+
+    private void Awake ()
+    {
+        TimeOfDay = Random.Range(0, 24);
+    }
+
+    private void Update() {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            TimeOfDay += Time.deltaTime * 0.13f;
+            TimeOfDay %= 24;
+            updateLighting(TimeOfDay / 24f);
+
+            // bright time last longer for like 2 times, this can be manipulated in settings
+            // lets say that I want a day to last 2 minutes and night to be 1 minute, right?
+            // than 2 minutes = 16
+            // 120 seconds = 16f
+        }
+    }
+
+    private void updateLighting(float timePercent)
+    {
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.AmbientColor.Evaluate(timePercent);
+
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+    }
+
+    private void onValidate()
+    {
+        if (DirectionalLight != null)
+            return;
+
+        if (RenderSettings.sun != null)
+        {
+            DirectionalLight = RenderSettings.sun;
+        }
+        else
+        {
+            Light[] lights = GameObject.FindObjectsOfType<Light>();
+
+            foreach (Light light in lights)
+            {
+                if (light.type == LightType.Directional)
+                {
+                    DirectionalLight = light;
+                    return;
+                }
+            }
+        }
+    }
+}
