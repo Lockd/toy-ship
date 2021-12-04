46abc22e947d3f38a65eddba900e9e52349d481b
Lockd <pikagrigoriy@gmail.com>
2021-12-05 00:14:46 +0300

    river generation settings are now adjustable

--- Assets/Scripts/Environment/RiverPathGenerator.cs
+++ Assets/Scripts/Environment/RiverPathGenerator.cs
@@ -7,13 +7,17 @@ public class RiverPathGenerator : MonoBehaviour
 {
     public PathCreator pathCreator;
     public float minDeltaX = .5f;
-    // public float minDeltaY;
+    public float maxDeltaX = .5f;
     public float minDeltaZ = .5f;
+    public float maxDeltaZ = .5f;
+    public float minDistanceFromRiverCurve = 1f;
+    public float maxDistanceFromRiverCurve = 3f;
     public float deltaForRiverSegments = 1f;
-    public float minDistanceFromRiverCurve = 1;
     public int lengthOfRiverSegment = 20;
 
     Vector3[] sideDots = null;
+    Vector3[] dotsLeft = null;
+    Vector3[] dotsRight = null;
 
     void Start()
     {
@@ -28,11 +32,9 @@ public class RiverPathGenerator : MonoBehaviour
         }
     }
 
-    // VertexPath GeneratePath(Vector2[] points, bool closedPath)
     BezierPath GeneratePath(Vector2[] points, bool closedPath)
     {
         BezierPath bezierPath = new BezierPath(points, closedPath, PathSpace.xz);
-        // return new VertexPath(bezierPath, transform);
         return bezierPath;
     }
 
@@ -43,15 +45,26 @@ public class RiverPathGenerator : MonoBehaviour
         pathCreator.bezierPath = path;
 
         sideDots = new Vector3[(int)(pathCreator.path.length / deltaForRiverSegments * 2 + 2)];
+        // Those arrays can be used for creating river height
+        dotsLeft = new Vector3[sideDots.Length / 2];
+        dotsRight = new Vector3[sideDots.Length / 2];
+
         int sideDotsIdx = 0;
         for (float i = 0; i <= pathCreator.path.length; i += deltaForRiverSegments)
         {
             Vector3 direction = pathCreator.path.GetDirectionAtDistance(i);
             Vector3 pos = pathCreator.path.GetPointAtDistance(i);
+
+            // TODO find a way to change width of the river :)
+            // right side of the river
             Vector3 rotatedLine = (Quaternion.AngleAxis(90, transform.up) * direction).normalized;
+            // Vector3 right = rotatedLine * Random.Range(minDistanceFromRiverCurve, maxDistanceFromRiverCurve);
             sideDots[sideDotsIdx] = pos + rotatedLine;
             sideDotsIdx++;
+
+            // left side of the river
             Vector3 rotatedLineNegative = (Quaternion.AngleAxis(-90, transform.up) * direction).normalized;
+            // Vector3 left = rotatedLineNegative * Random.Range(minDistanceFromRiverCurve, maxDistanceFromRiverCurve);
             sideDots[sideDotsIdx] = pos + rotatedLineNegative;
             sideDotsIdx++;
         }
@@ -63,7 +76,9 @@ public class RiverPathGenerator : MonoBehaviour
         points[0] = new Vector2(0f, 0f);
         for (int i = 1; i < lengthOfRiverSegment; i++)
         {
-            points[i] = new Vector2(Random.Range(-3f, 3f), Random.Range(2f, 5f)) + points[i - 1];
+            float x = Random.Range(minDeltaX, maxDeltaX);
+            float z = Random.Range(minDeltaZ, maxDeltaZ);
+            points[i] = new Vector2(x, z) + points[i - 1];
         }
         return points;
     }
