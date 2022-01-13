eaf07811a387fc30bea677bed1c02f5c3d95fa08
Lockd <pikagrigoriy@gmail.com>
2022-01-13 10:25:39 +0300

    updated fox controller and animal behaviour script

--- Assets/Scripts/Animals/AnimalBehaviour.cs
+++ Assets/Scripts/Animals/AnimalBehaviour.cs
@@ -7,17 +7,23 @@ public class AnimalBehaviour : MonoBehaviour
 {
     public Animator animator;
     public PathCreator pathCreator;
+    public float speed;
     public float minDistanceToPlayer = 18f;
     public bool startDigging = false;
     public bool isSleeping = false;
+    public bool isRunning = false;
     GameObject player;
     float distanceTravelled = 0f;
     ThirdPersonMovement thirdPersonMovement;
-    bool isNearPlayer = false;
-    List<string> runningAnimations = new List<string>{ "Fox_Run", "Rab_Run", "Rac_Run Forward" };
+    List<string> animationsWithMovement;
 
     void Start()
     {
+        animationsWithMovement = new List<string>{
+            "Fox_Run", "Rab_Run", "Rac_Run Forward",
+            "Fox_Jump_Run", "Rab_Jump_Run", "Rac_Jump Run"
+        };
+
         player = GameObject.Find("Player");
         thirdPersonMovement = player.GetComponent<ThirdPersonMovement>();
 
@@ -30,31 +36,40 @@ public class AnimalBehaviour : MonoBehaviour
         {
             animator.SetBool("isSleeping", true);
         }
+
+        if (isRunning)
+        {
+            animator.SetTrigger("Run");
+        }
     }
 
     void Update()
     {
         if (player != null && thirdPersonMovement != null)
         {
-            if (transform.position.z - player.transform.position.z < minDistanceToPlayer && !isNearPlayer && pathCreator != null)
+            if (transform.position.z - player.transform.position.z < minDistanceToPlayer && !isRunning && pathCreator != null)
             {
-                animator.SetBool("isNearPlayer", true);
-                isNearPlayer = true;
+                animator.SetTrigger("Run");
+                isRunning = true;
             }
 
             AnimatorClipInfo[] clips = animator.GetCurrentAnimatorClipInfo(0);
-            
-            if (isNearPlayer && runningAnimations.Contains(clips[0].clip.name))
+            if (clips.Length > 0)
             {
-                distanceTravelled += (thirdPersonMovement.speed + 3f) * Time.deltaTime;
-                transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
-                transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+                if (isRunning && animationsWithMovement.Contains(clips[0].clip.name))
+                {
+                    float movementSpeed = speed != 0 ? speed : thirdPersonMovement.speed;
+                    distanceTravelled += (movementSpeed + 3f) * Time.deltaTime;
+                    transform.position = pathCreator.path.GetPointAtDistance(distanceTravelled);
+                    transform.rotation = pathCreator.path.GetRotationAtDistance(distanceTravelled);
+                }
             }
         }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        // Jumping does not work properly when following pathCreator
         if (other.tag == "Animal_jump_collider")
         {
             animator.SetTrigger("Jump");
