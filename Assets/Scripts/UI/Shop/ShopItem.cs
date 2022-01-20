f6793bfca65e4829a84b338a8d871378233c6b36
Lockd <pikagrigoriy@gmail.com>
2022-01-20 09:52:47 +0300

    fixed shop item behaviour

--- Assets/Scripts/UI/Shop/ShopItem.cs
+++ Assets/Scripts/UI/Shop/ShopItem.cs
@@ -1,17 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopItem : MonoBehaviour
 {
     public int price;
+    Button button;
     bool isPurchased = false;
     CoinsUI CoinsUI;
-    
+    string GameObjectStoreName;
+
     void Start()
     {
+        GameObjectStoreName = "is" + gameObject.name + "Purchased";
         GameObject header = GameObject.Find("Coins Text");
+
+        button = gameObject.GetComponent<Button>();
+
         CoinsUI = header.GetComponent<CoinsUI>();
+        int wasPurchased = PlayerPrefs.GetInt(GameObjectStoreName);
+        if (wasPurchased > 0)
+        {
+            isPurchased = true;
+            button.interactable = false;
+        }
     }
 
     void Update()
@@ -22,7 +35,17 @@ public class ShopItem : MonoBehaviour
     public void onPurchase()
     {
         float coinsAmount = PlayerPrefs.GetFloat("Coins");
-        PlayerPrefs.SetFloat("Coins", coinsAmount - price);
-        CoinsUI.updateCoinsInUI();
+        if (coinsAmount >= price && !isPurchased)
+        {
+            PlayerPrefs.SetFloat("Coins", coinsAmount - price);
+            isPurchased = true;
+            button.interactable = false;
+            PlayerPrefs.SetInt(GameObjectStoreName, 1);
+            CoinsUI.updateCoinsInUI();
+        }
+        else
+        {
+            // TODO display tolltip error about not enough money
+        }
     }
 }
