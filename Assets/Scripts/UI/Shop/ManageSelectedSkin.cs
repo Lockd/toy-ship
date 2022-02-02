7b54126f8229589ce13cc05a069b82af96f94909
Lockd <pikagrigoriy@gmail.com>
2022-02-02 23:29:44 +0300

    skins are now changable

--- Assets/Scripts/UI/Shop/ManageSelectedSkin.cs
+++ Assets/Scripts/UI/Shop/ManageSelectedSkin.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class ManageSelectedSkin : MonoBehaviour
 {
-    Vector3 skinPosition = new Vector3(0f, 0f, -124f);
+    Vector3 skinPosition = new Vector3(0f, -76f, -124f);
 
     public void updateSelectedSkin(ShipSkin newSkin)
     {
-        GameObject skin = Instantiate(newSkin.model, skinPosition, transform.rotation);
+        foreach (Transform child in transform) {
+            GameObject.Destroy(child.gameObject);
+        }
+        
+        GameObject skin = Instantiate(newSkin.model, new Vector3(0f, 0f, 0f), transform.rotation);
+        
+        // Layer 5 for UI
+        skin.layer = 5;
+        foreach(Transform child in skin.transform)
+        {
+            child.gameObject.layer = 5;
+        }
+
+        skin.transform.parent = gameObject.transform;
+        
         skin.AddComponent<SelectedSkinBehaviour>();
+        RectTransform rectTransform = skin.AddComponent<RectTransform>();
+
+        rectTransform.anchoredPosition = skinPosition;
     }
 }
